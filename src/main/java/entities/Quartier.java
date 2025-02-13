e4e1a0ecdf62f9309befd1d7233253e9090e12e6
ismailchaabane <ichaabane6@gmail.com>
2025-02-13 20:18:16 +0100

    ajout de l'interface login et forget password

--- src/main/java/entities/Quartier.java
+++ src/main/java/entities/Quartier.java
@@ -1,17 +1,32 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Quartier {
     private int id;
     private int nbLamp;
     private double consom_tot;
+    private List<Lampadaire> lampadaires;
 
     // Constructeurs
-    public Quartier() {}
+    public Quartier() {
+        this.lampadaires = new ArrayList<>();
+    }
 
     public Quartier(int id, int nbLamp, double consom_tot) {
         this.id = id;
         this.nbLamp = nbLamp;
         this.consom_tot = consom_tot;
+        this.lampadaires = new ArrayList<>();
+
+    }
+
+    public Quartier(int id) {
+        this.id = id;
+        this.lampadaires = new ArrayList<>();
+
     }
 
     // Getters et Setters
@@ -37,6 +52,20 @@ public class Quartier {
 
     public void setConsom_tot(double consom_tot) {
         this.consom_tot = consom_tot;
+    }
+
+    public List<Lampadaire> getLampadaires() {
+        return lampadaires;
+    }
+
+    public void setLampadaires(List<Lampadaire> lampadaires) {
+        this.lampadaires = lampadaires;
+    }
+
+    // Méthode pour ajouter un lampadaire au quartier
+    public void addLampadaire(Lampadaire lampadaire) {
+        this.lampadaires.add(lampadaire);
+        lampadaire.setQuartier(this); // Définir le quartier du lampadaire
     }
 
     // Méthode toString pour afficher les informations du quartier
--- src/main/java/test/MainClass.java
+++ src/main/java/test/MainClass.java
@@ -1,15 +1,24 @@
 package test;
 
 import entities.*;
+import javafx.fxml.FXMLLoader;
 import services.*;
+import entities.Lampadaire;
+import entities.Quartier;
+import services.LampadaireService;
+import services.QuartierService;
+import tools.MyConnection;
+import java.util.List;
 
 public class MainClass {
     public static void main(String[] args){
+        //FXMLLoader loader=new FXMLLoader(ge)
 
         //declarations des entites
         DossierFiscale dossier = new DossierFiscale(1, 13, 2024, 1000.0, 200.0, "En cours", "2025-02-05", "Carte bancaire");
         DossierFiscale dossier1 = new DossierFiscale(1, 14, 2025, 1000.0, 600.0, "En cours", "2025-02-05", "Carte bancaire");
         DossierFiscale dossier2 = new DossierFiscale(2, 13, 2025, 100000.0, 10.0, "En cours", "2025-02-05", "cash");
+        DossierFiscale dossier3 = new DossierFiscale(3, 13, 2025, 100000.0, 10.0, "En cours", "2025-02-05", "cash");
         DeclarationRevenues dr= new DeclarationRevenues(1,1,200.0,"esprit","2025-06-04","/fichier");
         DeclarationRevenues dr1= new DeclarationRevenues(1,1,500000,"saraya","2025-06-04","/fichier");
 
@@ -40,11 +49,11 @@ public class MainClass {
 
 
         //exporter
-        dossierService.ExportPDF(dossier2);
+        dossierService.ExportPDF(dossier3);
         dossierService.ExportExcel("data");
 
 
-        
+
 //        Lampadaire p=new Lampadaire(3, "Rue de Bourguiba", true, 150.3);
 //        Quartier q=new Quartier(2,4,150.0);
 //        LampadaireService ps=new LampadaireService();
